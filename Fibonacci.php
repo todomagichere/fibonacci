7c0958d6ac53cdc885359582d59d39f3413e3efd
Roberto Pérez <roberto.perez@alfa9.com>
2017-02-20 10:45:58 +0100

    Getters... setters... construct... POO stuff

---
+++ Fibonacci.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Lzr\Fibonnacci;
+
+use \Exception;
+
+class Fibonacci
+{
+    private $maxNumber = null;
+    private $sequence = null;
+
+    public function __construct($maxNumber)
+    {
+        $this->setMaxNumber($maxNumber);
+    }
+
+    public function getSequence()
+    {
+        if (is_null($this->sequence)) {
+            throw new Exception('Sequence not initialized');
+        }
+        return $this->sequence;
+    }
+
+    public function setMaxNumber($maxNumber)
+    {
+        if (!is_int($maxNumber)) {
+            throw new Exception('The max number must be a natural number');
+        }
+        $this->maxNumber = $maxNumber;
+    }
+
+    public function getMaxNumber()
+    {
+        if (is_null($this->maxNumber)) {
+            throw new Exception('Max number not initialized');
+        }
+        return $this->maxNumber;
+    }
+
+
+}
