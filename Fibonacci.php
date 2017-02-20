76f8bea84b751f10abfc5a3cec5493931465188a
Roberto Pérez <roberto.perez@alfa9.com>
2017-02-20 16:10:10 +0100

    Using different complexity type to get the nth number

--- Fibonacci.php
+++ Fibonacci.php
@@ -12,17 +12,12 @@ class Fibonacci
     public function __construct($maxNumber = null)
     {
         $this->maxNumber = $maxNumber;
+        $this->checkNumber();
     }
 
     public function getSequence()
     {
-        if (is_null($this->maxNumber)) {
-            throw new Exception('Max number not initialized');
-        }
-        if (!$this->sequence) {
-            throw new Exception('Sequence not initialized');
-        }
-        return $this->sequence;
+        //TODO magic here
     }
 
     public function initSequence()
@@ -44,5 +39,93 @@ class Fibonacci
             throw new Exception('Max number not initialized');
         }
         return $this->maxNumber;
+    }
+
+    public function checkNumber()
+    {
+        //TODO magic here
+    }
+
+    /**
+     * Get the nth Fibonacci number O(Fi^n)
+     * @param integer $n
+     * @return integer
+     * @throws Exception
+     */
+    private function getElementExponentialComplexity($n)
+    {
+        if ($n < 2) {
+            return $n;
+        } else {
+            return ($this->getElementExponentialComplexity($n - 1) + $this->getElementExponentialComplexity($n - 2));
+        }
+    }
+
+    private function getElementLinearComplexity($n)
+    {
+        $i = 1;
+        $j = 0;
+        $counter = $n - 1;
+        for ($k = 0; $k <= $counter; $k++) {
+            $t = $i + $j;
+            $j = $i;
+            $i = $t;
+        }
+        return $j;
+    }
+
+    private function getElementLogarithmicComplexity($n)
+    {
+        if ($n <= 0) {
+            return 0;
+        }
+        $i = $n - 1;
+        $auxOne = 0;
+        $auxTwo = 1;
+        $a = $auxTwo;
+        $b = $auxOne;
+        $c = $auxOne;
+        $d = $auxTwo;
+        while ($i > 0) {
+            if ($i % 2 != 0) {
+                $auxOne = (($d*$b) + ($c*$a));
+                $auxTwo = ($d*($b + $a) + $c*$b);
+                $a = $auxOne;
+                $b = $auxTwo;
+            }
+            $auxOne = (pow($c, 2) + pow($d, 2));
+            $auxTwo = ($d*(2*$c + $d));
+            $c = $auxOne;
+            $d = $auxTwo;
+            $i = $i / 2;
+        }
+        return ($a + $b);
+    }
+
+    /**
+     * Get the nth fibonacci number
+     * using a determinate computational complexity
+     *
+     * @param integer $n nth element to get
+     * @param string $complexity ['logarithmic' | 'linear' | 'exponential']
+     * @return integer
+     * @throws Exception If complexity is not defined, throw an exception
+     */
+    public function getElement($n, $complexity = 'logarithmic')
+    {
+        if ($n < 0) {
+            throw new Exception($n . ' is not a valid index');
+        }
+
+        switch ($complexity) {
+            case 'logarithmic':
+                return $this->getElementLogarithmicComplexity($n);
+            case 'exponential':
+                return $this->getElementExponentialComplexity($n);
+            case 'linear':
+                return $this->getElementLinearComplexity($n);
+            default:
+                throw new Exception('complexity not defined');
+        }
     }
 }
